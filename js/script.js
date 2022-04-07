8873901e7497e850bc62c4985d9d8b2d3a2c3189
Kestrel.Bryce <99274358+KestrelBryce@users.noreply.github.com>
2022-04-07 03:09:14 +0000

    Created Super linter after finishing this at 11pm :(

--- js/script.js
+++ js/script.js
@@ -3,17 +3,21 @@
 
 function Calculate_area() {
   // input
-  let Side_a = document.getElementById('Side-a-here').value;
-  let Side_b = document.getElementById('Side-b-here').value;
-  let Height = document.getElementById('Height-here').value;
+  let Side_a = parseInt( (document.getElementById('Side-a-here').value));
+  let Side_b = parseInt( (document.getElementById('Side-b-here').value));
+  let Height = parseInt((document.getElementById('Height-here').value));
   
   // naming constants and stuff
-  const Half_rate = 0.5
-  let Trapezoid_area = (Side_a + Side_b) * Half_rate * Height;
-
+  const Half_rate = 0.5;
+  
+  let Trapezoid_area = (Side_a + Side_b) * 0.5 * Height;
   // remember to use .toFixed(2) to round to two decimal places
   
   // output for area
   document.getElementById('Trapezoid-results').innerHTML = "Your    area rounded to the nearest hundreth is " +            Trapezoid_area.toFixed(2) + "cm."
 
+
+ // document.getElementById('Trapezoid-results').innerHTML = "Your    area rounded to the nearest hundreth is " +            Trapezoid_area.toFixed(2) + "cm."
+
+  
 }
